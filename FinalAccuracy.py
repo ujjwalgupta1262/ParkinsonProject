e5be8f6b3339222659c66b15235fc983a09b5220
Ujjwal Gupta <ujjwalgupta1262@gmail.com>
2018-05-05 00:52:54 +0530

    Update FinalAccuracy.py

--- FinalAccuracy.py
+++ FinalAccuracy.py
@@ -6,7 +6,11 @@ import math
 def combine(X,y):
     temp = []
     for i in range(len(X)):
-        temp.append([X[i],y[i]])
+        if(X[i] != []):
+            temp.append([X[i],y[i]])
+        else:
+            temp.append([y[i]])
+
     return temp
 
 def separate(list):
@@ -59,130 +63,168 @@ def getAccuracy(X, y):
     acc=max_score/10.0
     return acc, max_score
 
-def getData(filename):
-	X_list = []
-	y_list = []
-	passed_vec = open(filename, "r").readlines()
-	for i in range(len(passed_vec)):
-		l[i] = l[1].split(",")
-		l[i][0] = l[i][0][3:-2]
-		temp_str = l[i][0]
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-		if (len(temp_str) > 7 and temp_str[0:8] == "trimMean"):
+
+def getData(filename):
+    X_list = [[]]*75
+    y_list = []
+    l = open(filename, 'r').readlines()
+    for i in range(len(l)):
+        l[i] = l[i].split(',')
+        l[i][0] = (l[i][0])[3:-2]
+        temp_str = l[i][0]
+        flag = 0
+        if len(temp_str) > 7 and temp_str[0:8] == 'trimMean':
             flag = 1
-            if (temp_str[9] == "_"):
+            if temp_str[9] == '_':
                 column = int(temp_str[8])
                 temp_str = temp_str[0:8] + temp_str[9:]
             else:
                 column = int(temp_str[8:10])
                 temp_str = temp_str[0:8] + temp_str[10:]
 
-        if (len(temp_str) > 10 and temp_str[0:11] == "percentiles"):
+        if len(temp_str) > 10 and temp_str[0:11] == 'percentiles':
             flag = 2
-            if (temp_str[12] == "_"):
+            if temp_str[12] == '_':
                 column = int(temp_str[11])
                 temp_str = temp_str[0:11] + temp_str[12:]
             else:
                 column = int(temp_str[11:13])
                 temp_str = temp_str[0:11] + temp_str[13:]
 
-        if (len(temp_str) > 6 and temp_str[0:7] == "moments"):
+        if len(temp_str) > 6 and temp_str[0:7] == 'moments':
             flag = 3
             column = int(temp_str[7])
             temp_str = temp_str[0:7] + temp_str[8:]
 
-         # print(temp_str)
-        temp_pd = temp_str[:-3] + "_pd" + temp_str[-3:]
-        # print(temp_pd)
-        temp_hc = temp_str[:-3] + "_hc" + temp_str[-3:]
+             # print(temp_str)
+
+        temp_pd = temp_str[:-3] + '_pd' + temp_str[-3:] + ".txt"
+
+            # print(temp_pd)
+
+        temp_hc = temp_str[:-3] + '_hc' + temp_str[-3:] + ".txt"
 
         try:
-            vec_pd = list(numpy.loadtxt(open(temp_pd, "rt"), delimiter="\n"))
-            #vec_pd = [x for x in vec_pd if math.isnan(x)==False]
-
+            vec_pd = list(numpy.loadtxt(open(temp_pd, 'rt'), delimiter='\n'
+                          ))
         except:
-            vec_pd = list(numpy.genfromtxt(open(temp_pd, "rt"), delimiter=" "))
-            #print(vec_pd)
-            #vec_pd = [x for x in vec_pd if math.isnan(x)==False]
-            # print(len(vec_pd[0]))
-# All the Nan values were considered as Zero as essentially their contribbution remains null and void
+
+                # vec_pd = [x for x in vec_pd if math.isnan(x)==False]
+
+            vec_pd = list(numpy.genfromtxt(open(temp_pd, 'rt'),
+                          delimiter=' '))
+
+                # print(vec_pd)
+                # vec_pd = [x for x in vec_pd if math.isnan(x)==False]
+                # print(len(vec_pd[0]))
+    # All the Nan values were considered as Zero as essentially their contribbution remains null and void
+
         try:
-            vec_hc = list(numpy.loadtxt(open(temp_hc, "rt"), delimiter="\n"))
-           # vec_hc = [x for x in vec_hc if math.isnan(x)==False]
+            vec_hc = list(numpy.loadtxt(open(temp_hc, 'rt'), delimiter='\n'
+                          ))
         except:
-            #print(temp_hc)
-            vec_hc = list(numpy.genfromtxt(open(temp_hc, "rt"), delimiter=" "))
-            #vec_hc = [x for x in vec_hc if math.isnan(x)==False]
-            # print(len(vec_hc[0]))
 
-        if (flag == 0):
+               # vec_hc = [x for x in vec_hc if math.isnan(x)==False]
+                # print(temp_hc)
+
+            vec_hc = list(numpy.genfromtxt(open(temp_hc, 'rt'),
+                          delimiter=' '))
+
+                # vec_hc = [x for x in vec_hc if math.isnan(x)==False]
+                # print(len(vec_hc[0]))
+
+        if flag == 0:
             X = vec_pd + vec_hc
+        elif flag == 1:
 
-        elif (flag == 1):
-            if (column == 5):
+            if column == 5:
                 X = get_column(vec_pd, 0) + get_column(vec_hc, 0)
-                X = [x for x in X if math.isnan(x)==False]
-            elif (column == 10):
+                X = [x for x in X if math.isnan(x) == False]
+            elif column == 10:
                 X = get_column(vec_pd, 1) + get_column(vec_hc, 1)
-                X = [x for x in X if math.isnan(x)==False]
-            elif (column == 20):
+                X = [x for x in X if math.isnan(x) == False]
+            elif column == 20:
                 X = get_column(vec_pd, 2) + get_column(vec_hc, 2)
-                X = [x for x in X if math.isnan(x)==False]
-            elif (column == 30):
+                X = [x for x in X if math.isnan(x) == False]
+            elif column == 30:
                 X = get_column(vec_pd, 3) + get_column(vec_hc, 3)
-                X = [x for x in X if math.isnan(x)==False]
-            elif (column == 40):
+                X = [x for x in X if math.isnan(x) == False]
+            elif column == 40:
                 X = get_column(vec_pd, 4) + get_column(vec_hc, 4)
-                X = [x for x in X if math.isnan(x)==False]
+                X = [x for x in X if math.isnan(x) == False]
+        elif flag == 2:
 
-        elif (flag == 2):
-            if (column == 1):
+            if column == 1:
                 X = get_column(vec_pd, 0) + get_column(vec_hc, 0)
-                X = [x for x in X if math.isnan(x)==False]
-            elif (column == 5):
+                X = [x for x in X if math.isnan(x) == False]
+            elif column == 5:
                 X = get_column(vec_pd, 1) + get_column(vec_hc, 1)
-                X = [x for x in X if math.isnan(x)==False]
-            elif (column == 10):
+                X = [x for x in X if math.isnan(x) == False]
+            elif column == 10:
                 X = get_column(vec_pd, 2) + get_column(vec_hc, 2)
-                X = [x for x in X if math.isnan(x)==False]
-            elif (column == 20):
+                X = [x for x in X if math.isnan(x) == False]
+            elif column == 20:
                 X = get_column(vec_pd, 3) + get_column(vec_hc, 3)
-                X = [x for x in X if math.isnan(x)==False]
-            elif (column == 25):
+                X = [x for x in X if math.isnan(x) == False]
+            elif column == 25:
                 X = get_column(vec_pd, 4) + get_column(vec_hc, 4)
-                X = [x for x in X if math.isnan(x)==False]
-            elif (column == 30):
+                X = [x for x in X if math.isnan(x) == False]
+            elif column == 30:
                 X = get_column(vec_pd, 5) + get_column(vec_hc, 5)
-                X = [x for x in X if math.isnan(x)==False]
-            elif (column == 90):
+                X = [x for x in X if math.isnan(x) == False]
+            elif column == 90:
                 X = get_column(vec_pd, 6) + get_column(vec_hc, 6)
-                X = [x for x in X if math.isnan(x)==False]
-            elif (column == 95):
+                X = [x for x in X if math.isnan(x) == False]
+            elif column == 95:
                 X = get_column(vec_pd, 7) + get_column(vec_hc, 7)
-                X = [x for x in X if math.isnan(x)==False]
-            elif (column == 99):
+                X = [x for x in X if math.isnan(x) == False]
+            elif column == 99:
                 X = get_column(vec_pd, 8) + get_column(vec_hc, 8)
-                X = [x for x in X if math.isnan(x)==False]
+                X = [x for x in X if math.isnan(x) == False]
+        elif flag == 3:
 
-        elif (flag == 3):
-            if (column == 3):
+            if column == 3:
                 X = get_column(vec_pd, 0) + get_column(vec_hc, 0)
-                X = [x for x in X if math.isnan(x)==False]
-            elif (column == 4):
+                X = [x for x in X if math.isnan(x) == False]
+            elif column == 4:
                 X = get_column(vec_pd, 1) + get_column(vec_hc, 1)
-                X = [x for x in X if math.isnan(x)==False]
-            elif (column == 5):
+                X = [x for x in X if math.isnan(x) == False]
+            elif column == 5:
                 X = get_column(vec_pd, 2) + get_column(vec_hc, 2)
-                X = [x for x in X if math.isnan(x)==False]
-            elif (column == 6):
+                X = [x for x in X if math.isnan(x) == False]
+            elif column == 6:
                 X = get_column(vec_pd, 3) + get_column(vec_hc, 3)
-                X = [x for x in X if math.isnan(x)==False]
+                X = [x for x in X if math.isnan(x) == False]
 
         y = [0] * len(vec_pd) + [1] * len(vec_hc)
-        X_list.append(X)
-        y_list.append(y)
+        X_list = combine(X_list,X)
 
-    return (X_list,y_list)
+    return (X_list, y)
+
+
+def getMaxAccuracy(filename):
+    data = getData(filename)
+    #print(len(data[0]))
+    X_list = data[0]
+    #print(X_list)
+    y = data[1]
+    max_score = 0
+    for i in range(len(X_list[0])):
+        feature = get_column(X_list,i)
+        if(i == 0):
+            features_X = feature
+        else:
+            features_X = combine(features_X,feature)
+        score = getAccuracy(features_X,y)
+        if(score > max_score):
+            max_score = score
+    return max_score
+
+
+print(getMaxAccuracy("test.txt"))
 
 
 
