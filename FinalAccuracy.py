a61501296de27d9e3c74e0de85f364f903d46965
Ujjwal Gupta <ujjwalgupta1262@gmail.com>
2018-05-17 23:44:42 +0800

    Update FinalAccuracy.py

--- FinalAccuracy.py
+++ FinalAccuracy.py
@@ -3,22 +3,31 @@ from sklearn import svm
 import random
 import math
 
+epochs = 50
+
+def normalise(list):
+    l = numpy.array(list)
+    mean = numpy.mean(l, axis = 0)
+    std = numpy.std(l,axis = 0)
+    return (l - mean)/std
+
+def getList(l,indices):
+    return [l[i] for i in indices]
+
 def combine(X,y):
     temp = []
     for i in range(len(X)):
-        if(X[i] != []):
-            temp.append([X[i],y[i]])
-        else:
-            temp.append([y[i]])
-
+        temp_list = list(X[i])
+        temp_list.append(y[i])
+        temp.append(temp_list)
     return temp
 
 def separate(list):
     X = []
     y = []
     for i in list:
-        X.append(i[0])
-        y.append(i[1])
+        X.append(i[:-1])
+        y.append(i[-1])
     return (X,y)
 
 def get_column(list, num_column):
@@ -45,9 +54,11 @@ def getAccuracy(X, y):
             # test sets
             X_test = X[alpha:beta]
             y_test = y[alpha:beta]
+            X_train = normalise(X_train)
+            X_test = normalise(X_test)
             # reshape and form arrays
-            X_train = numpy.asarray(X_train).reshape(-1, 1)
-            X_test = numpy.asarray(X_test).reshape(-1, 1)
+            #X_train = numpy.asarray(X_train).reshape(-1, 1)
+            #X_test = numpy.asarray(X_test).reshape(-1, 1)
             # X_test_set = numpy.asarray(X_test_set).reshape(-1,1)
             # reshape and form arrays
             y_train = numpy.asarray(y_train)
@@ -205,26 +216,76 @@ def getData(filename):
     return (X_list, y)
 
 
-def getMaxAccuracy(filename):
+def getMaxAccuracyTotal(filename):
     data = getData(filename)
-    #print(len(data[0]))
     X_list = data[0]
-    #print(X_list)
+    features_X = [[]]*len(X_list)
     y = data[1]
     max_score = 0
     for i in range(len(X_list[0])):
         feature = get_column(X_list,i)
-        if(i == 0):
-            features_X = feature
-        else:
-            features_X = combine(features_X,feature)
-        score = getAccuracy(features_X,y)
+        features_X = combine(features_X,feature)
+        tot_score = 0
+        for i in range(epochs):
+            t = combine(features_X,y)
+            random.shuffle(t)
+            (features_X,y) = separate(t)
+            tot_score += getAccuracy(features_X,y)[0]
+        score = tot_score/epochs
         if(score > max_score):
             max_score = score
     return max_score
 
+def getMaxAccuracyMale(filename):
+    data = getData(filename)
+    index_pd = [8, 9, 10, 11, 13, 20, 21, 23, 24, 25, 27, 28, 29, 30, 31, 32, 33, 34, 35]
+    index_hc = [1, 4, 5, 7, 8, 9, 10, 13, 14, 18, 21, 22, 24, 25, 29, 30, 31, 34, 35, 37]
+    X_list = getList(data[0],index_hc) + getList(data[0],index_pd)
+    features_X = [[]]*len(X_list)
+    y = [0]*len(index_hc) + [1]*len(index_pd)
+    #print(y)
+    max_score = 0
+    for i in range(len(X_list[0])):
+        feature = get_column(X_list,i)
+        features_X = combine(features_X,feature)
+        tot_score = 0
+        for i in range(epochs):
+            t = combine(features_X,y)
+            random.shuffle(t)
+            (features_X,y) = separate(t)
+            tot_score += getAccuracy(features_X,y)[0]
+        score = tot_score/epochs
+        score = getAccuracy(features_X,y)[0]
+        if(score > max_score):
+            max_score = score
+    return max_score  
 
-print(getMaxAccuracy("test.txt"))
+def getMaxAccuracyFemale(filename):
+    data = getData(filename)
+    index_pd = [0, 1, 2, 3, 4, 5, 6, 7, 12, 14, 15, 16, 17, 18, 19, 22, 26, 36]
+    index_hc = [0, 2, 3, 6, 11, 12, 15, 16, 17, 19, 20, 23, 26, 27, 28, 32, 33, 36]
+    X_list = getList(data[0],index_hc) + getList(data[0],index_pd)
+    features_X = [[]]*len(X_list)
+    y = [0]*len(index_hc) + [1]*len(index_pd)
+    #print(y)
+    max_score = 0
+    for i in range(len(X_list[0])):
+        feature = get_column(X_list,i)
+        features_X = combine(features_X,feature)
+        tot_score = 0
+        for i in range(epochs):
+            t = combine(features_X,y)
+            random.shuffle(t)
+            (features_X,y) = separate(t)
+            tot_score += getAccuracy(features_X,y)[0]
+        score = tot_score/epochs
+        if(score > max_score):
+            max_score = score
+    return max_score 
+
+
+print(getMaxAccuracyFemale("test.txt"))
+
 
 
 
